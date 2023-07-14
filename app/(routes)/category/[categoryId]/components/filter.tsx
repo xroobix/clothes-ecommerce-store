c463515a5a815e93b25d465854202058ab01c3cd
xroobix <109462535+xroobix@users.noreply.github.com>
2023-07-14 22:44:58 +0200

    Adding filters for products in category page

---
+++ app/(routes)/category/[categoryId]/components/filter.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+export const revalidate = 0;
+
+import Button from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Color, Size } from '@/types';
+import { useRouter, useSearchParams } from 'next/navigation';
+import qs from 'query-string';
+
+interface FilterProps {
+  data: (Size | Color)[];
+  name: string;
+  valueKey: string;
+}
+
+const Filter: React.FC<FilterProps> = ({ data, name, valueKey }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const selectedValue = searchParams.get(valueKey);
+
+  const onClick = (id: string) => {
+    const current = qs.parse(searchParams.toString());
+    const query = { ...current, [valueKey]: id };
+
+    if (current[valueKey] === id) {
+      query[valueKey] = null;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  };
+
+  return (
+    <div className="mb-8">
+      <h3 className="text-lg font-semibold">{name}</h3>
+      <hr className="my-4" />
+      <div className="flex flex-wrap gap-2">
+        {data.map((filter) => (
+          <div key={filter.id} className="flex items-center">
+            <Button
+              className={cn(
+                'text-sm rounded-md text-slate-800 border p-2 border-slate-300 bg-white',
+                selectedValue === filter.id && 'bg-black'
+              )}
+              onClick={() => onClick(filter.id)}
+            >
+              {filter.name}
+            </Button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,10 +5,11 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, disabled, type = 'button', ...propTypes }, ref) => {
+  ({ className, children, disabled, type = 'button', ...props }, ref) => {
     return (
       <button
         ref={ref}
+        {...props}
         className={cn(
           `w-auto rounded-full bg-slate-950 border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`,
           className
