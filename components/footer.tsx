5392a094f5a5ad64fe2c6fdf4f8d1f25729e822b
xroobix <109462535+xroobix@users.noreply.github.com>
2023-07-14 16:59:41 +0200

    Addinghome page with label and featured products

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-slate-900">
+          &copy; 2023 ClothingStore, Inc. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/ui/product-card.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import IconButton from '@/components/ui/icon-button';
+import { Product } from '@/types';
+import { Expand, ShoppingCart } from 'lucide-react';
+import Image from 'next/image';
+import Currency from '@/components/ui/currency';
+
+interface ProductCardProps {
+  data: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  return (
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      {/* Images and Actions */}
+      <div className="aspect-square rounded-xl bg-slate-100 relative">
+        <Image
+          alt="image"
+          src={data?.images?.[0]?.url}
+          fill
+          className="aspect-square object-cover rounded-md"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton
+              icon={<Expand size={20} className="text-slate-600" />}
+            />
+            <IconButton
+              icon={<ShoppingCart size={20} className="text-slate-600" />}
+            />
+          </div>
+        </div>
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-slate-500">{data.category?.name}</p>
+      </div>
+      {/* Price */}
+      <div className="flex items-center justify-between">
+        <Currency value={data.price} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
