50f3be08c4d136cb0afac522fa4b016013328c10
xroobix <109462535+xroobix@users.noreply.github.com>
2023-07-18 01:58:01 +0200

    Adding skeletons and loading components

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -6,7 +6,7 @@ interface BillboardProps {
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
-    <div className="p-4 sma:p-6 lg:p-8 rounded-xl overflow-hidden">
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
         style={{ backgroundImage: `url(${data?.imageURL})` }}
         className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,6 +9,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         ref={ref}
+        disabled={disabled}
         {...props}
         className={cn(
           `w-auto rounded-full bg-slate-950 border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`,
