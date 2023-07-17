d1d83dee11b58efcb744c86da3e4dc3225c0212e
xroobix <109462535+xroobix@users.noreply.github.com>
2023-07-18 01:21:18 +0200

    Fixing style

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -17,7 +17,7 @@ const CartPage = () => {
       <Container>
         <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-slate-950">Shopping Cart</h1>
-          <div className='mt-12 lg:grid lg:grid-col-12 lg:items-start gap-x-12'>
+          <div className='mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12'>
             <div className='lg:col-span-7'>
                 {cart.items.length === 0 && <p className='text-slate-500'>No items added to cart</p>}
                 <ul>
